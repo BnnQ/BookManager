9f79aef4a8499f12695745dca231f87394f75ee1
BnnQ <1v25fa94@gmail.com>
2023-05-19 01:13:53 +0300

    Initial commit

---
+++ Utils/Extensions/StartupExtensions.cs
@@ -0,0 +1,78 @@
+using System.Text.Json;
+using BookManager.Models;
+using BookManager.Services.Abstractions;
+using BookManager.Services;
+using BookManager.Services.MapperProfiles;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Cosmos.Fluent;
+
+namespace BookManager.Utils.Extensions;
+
+public static class StartupExtensions
+{
+    public static WebApplicationBuilder ConfigureServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.Configure<Configuration.Azure>(builder.Configuration.GetRequiredSection("Azure"));
+
+        builder.Services.AddControllersWithViews();
+
+        builder.Services.AddSingleton<JsonSerializerOptions>(_ => new JsonSerializerOptions
+            { WriteIndented = false, PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
+
+        builder.Services.AddSingleton<CosmosSerializationOptions>(_ => new CosmosSerializationOptions
+            { Indented = false, PropertyNamingPolicy = CosmosPropertyNamingPolicy.CamelCase });
+
+        builder.Services.AddSingleton<CosmosClient>(serviceProvider =>
+        {
+            var connectionString = builder.Configuration.GetConnectionString("CosmosAccount") ??
+                                   throw new InvalidOperationException("'CosmosAccount' connection string is not provided.");
+
+            var serializerOptions = serviceProvider.GetRequiredService<CosmosSerializationOptions>();
+            var cosmosClientBuilder = new CosmosClientBuilder(connectionString).WithSerializerOptions(serializerOptions);
+
+            return cosmosClientBuilder.Build();
+        });
+
+        builder.Services.AddTransient<IIdentifierGenerator, GuidIdentifierGenerator>();
+        builder.Services.AddSingleton<CosmosAccountClient>();
+        builder.Services.AddTransient<IRepository<Book, string>, CosmosBookRepository>();
+
+        builder.Services.AddAutoMapper(profiles => { profiles.AddProfile<BookMapperProfile>(); });
+
+        builder.Services.AddCors(options =>
+        {
+            options.AddDefaultPolicy(policyBuilder =>
+            {
+                policyBuilder//.WithOrigins("https://localhost:44476")
+                    .AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+                //.AllowCredentials();
+            });
+        });
+
+
+        return builder;
+    }
+
+    public static void Configure(this WebApplication app)
+    {
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+        app.UseRouting();
+
+        app.UseCors();
+
+        app.MapControllers();
+
+        app.MapFallbackToFile("index.html");
+    }
+    
+}
